79f3da9ad508b4bb9c2ce5207ee0858847035095
Наталья Скороспелова <soltk7@yandex.ru>
2022-12-01 12:15:45 +0300

    Исправлена ошибка в 4 задаче

--- 3hw4.py
+++ 3hw4.py
@@ -1,4 +1,5 @@
 import random
+from math import floor
 
 n=abs(int(input('укажите количество чисел ')))
 if n == 0:
@@ -7,7 +8,8 @@ else:
     my_list = []
     while(len(my_list)<n):
         my_list.append(round((random.randint(1,1000)/100),2))
-        k = my_list[len(my_list)-1]
+        k = round((my_list[len(my_list)-1] - floor(my_list[len(my_list)-1])),2)
+
         if len(my_list)-1 == 0:
             min = k
             max = k    
